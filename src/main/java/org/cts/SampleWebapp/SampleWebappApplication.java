3fa6c813c30012b15f7f54b66028be5b2f3d5d73
ram <ram@gmail.com>
2020-01-04 14:32:02 +0530

    Initial commit

---
+++ src/main/java/org/cts/SampleWebapp/SampleWebappApplication.java
@@ -0,0 +1,13 @@
+package org.cts.SampleWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cts/SampleWebapp/SampleWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cts.SampleWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
